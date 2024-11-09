fc75e426b5e47be1bef718a8acfe69bb933aad83
Manu16 <116045677+ManuD33P@users.noreply.github.com>
2024-11-09 06:02:45 -0300

    clear console.log

--- src/components/Cloudinary/UploadWidget.jsx
+++ src/components/Cloudinary/UploadWidget.jsx
@@ -2,11 +2,6 @@ import { CldUploadWidget } from 'next-cloudinary';
  
 
 export default ({onSuccess}) => {
-    const {NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,NEXT_PUBLIC_CLOUDINARY_API_KEY,NEXT_PUBLIC_CLOUDINARY_API_SECRET} = process.env
-    // console.log('valor del env',NEXT_PUBLIC_CLOUDINARY_API_KEY,NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,NEXT_PUBLIC_CLOUDINARY_API_SECRET)
-    // En cualquier componente de React/Next.js
-console.log('Cloudinary Cloud Name1:', NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME)
-console.log('Cloudinary Cloud Name:', process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME);
 
     return(
         <CldUploadWidget 
